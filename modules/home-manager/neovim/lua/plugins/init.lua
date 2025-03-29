c0fd11d49b27f0d4680ef2b624749e5c1adcab70
BirdeeHub <birdee@localhost>
2025-03-29 11:53:03 -0700

    Fix: a bunch of invalid specs, and an extra require basically

--- modules/home-manager/neovim/lua/plugins/init.lua
+++ modules/home-manager/neovim/lua/plugins/init.lua
@@ -1,9 +1,6 @@
-return {require("lze").load {{
-    import = "plugins.telescope"
-}, {
-    import = "plugins.treesitter"
-}, {
-    import = "plugins.completion"
-}}, {
-    import = "plugins.lualine"
-}}
+return {
+    { import = "plugins.telescope" },
+    { import = "plugins.treesitter" },
+    { import = "plugins.completion" },
+    { import = "plugins.lualine" }
+}
--- modules/home-manager/neovim/lua/plugins/lualine.lua
+++ modules/home-manager/neovim/lua/plugins/lualine.lua
@@ -1,15 +1,21 @@
-return {{require('lualine').setup {
-    options = {
-        theme = 'auto',
-        laststatus = 2,
-        globalstatus = true
-    }, -- Default theme
-    sections = {
-        lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff'},
-        lualine_c = {'filename'},
-        lualine_x = {'filetype'},
-        lualine_y = {'progress'},
-        lualine_z = {'location'}
-    }
-}}}
+return {
+    "lualine.nvim",
+    event = { "DeferredUIEnter" },
+    after = function(_)
+      require('lualine').setup {
+        options = {
+          theme = 'auto',
+          laststatus = 2,
+          globalstatus = true
+        },   -- Default theme
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff' },
+          lualine_c = { 'filename' },
+          lualine_x = { 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' }
+        }
+      }
+    end,
+}
