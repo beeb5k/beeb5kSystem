6f86e58d05e0f26a42a4d41f5951b10dc0d8e0c9
beeb5k <beeb5k6@gmail.com>
2025-05-01 23:25:45 +0530

    meow

--- modules/home-manager/neovim/lua/sets.lua
+++ modules/home-manager/neovim/lua/sets.lua
@@ -1,41 +1,70 @@
--- Performance
-vim.loader.enable()
-vim.opt.updatetime = 50
+-- ================================
+-- ⚡ Performance
+-- ================================
+vim.loader.enable()             -- Enable faster Lua module loading
+vim.opt.updatetime = 50        -- Faster completion and CursorHold events
 
--- User Interface
-vim.opt.guicursor = ""
-vim.opt.termguicolors = true
-vim.opt.showmode = false
-vim.opt.signcolumn = "yes"
-vim.opt.colorcolumn = "85"
+-- ================================
+-- 🖥️ User Interface
+-- ================================
+vim.opt.guicursor = ""         -- Use block cursor
+vim.opt.termguicolors = true   -- Enable 24-bit RGB colors
+vim.opt.showmode = false       -- Don't show mode (handled by statusline)
+vim.opt.showcmd = false        -- Hide command in the last line
+vim.opt.signcolumn = "yes"     -- Always show sign column
+vim.opt.colorcolumn = "85"     -- Highlight column 85
+vim.opt.cursorline = true      -- Highlight current line
 
--- Line Numbers
-vim.opt.nu = true
-vim.opt.relativenumber = true
-vim.opt.cursorline = true
+-- ================================
+-- 🔢 Line Numbers
+-- ================================
+vim.opt.nu = true              -- Absolute line numbers
+vim.opt.relativenumber = true  -- Relative line numbers
 
--- Mouse and Cursor
-vim.opt.mouse = ""
-vim.o.scrolloff = 20
+-- ================================
+-- 🖱️ Mouse and Scrolling
+-- ================================
+vim.opt.mouse = ""             -- Disable mouse support
+vim.opt.scrolloff = 20         -- Keep 20 lines above/below cursor
 
--- Leader Key
-vim.g.mapleader = " "
+-- ================================
+-- 🔑 Keybindings
+-- ================================
+vim.g.mapleader = " "          -- Set leader key to Space
 
--- File Explorer
-vim.g.loaded_netrwPlugin = 1
-vim.opt.isfname:append("@-@")
+-- ================================
+-- 📁 File Explorer & File Handling
+-- ================================
+vim.g.loaded_netrwPlugin = 1   -- Disable netrw file explorer
+vim.opt.isfname:append("@-@")  -- Allow @ in filenames
 
--- Indentation and Tabs
-vim.o.expandtab = true
-vim.o.smartindent = true
-vim.o.tabstop = 4
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
+-- ================================
+-- 🔤 Indentation and Tabs
+-- ================================
+vim.opt.expandtab = true       -- Convert tabs to spaces
+vim.opt.tabstop = 4            -- Number of spaces per tab
+vim.opt.softtabstop = 4        -- Number of spaces for soft tab
+vim.opt.shiftwidth = 4         -- Number of spaces for indent
+vim.opt.smartindent = true     -- Smart autoindenting
+vim.opt.smarttab = true        -- Use shiftwidth at beginning of lines
+vim.opt.breakindent = true     -- Wrap indent to match line start
+vim.opt.backspace = { "indent", "eol", "start" }  -- Allow backspace through everything
+vim.opt.autoindent = true      -- Copy indent from current line when starting new one
 
--- Text Wrapping
-vim.o.wrap = false
-vim.o.linebreak = true
+-- ================================
+-- 🔃 Text Wrapping
+-- ================================
+vim.opt.wrap = false           -- Disable line wrapping
+vim.opt.linebreak = true       -- Wrap long lines at word boundaries
 
--- Search
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+-- ================================
+-- 🔍 Search
+-- ================================
+vim.opt.hlsearch = false       -- Don't highlight search results
+vim.opt.incsearch = true       -- Show search matches as you type
+
+-- ================================
+-- 🏷️ Misc
+-- ================================
+vim.opt.title = true           -- Set terminal title to file name
+
