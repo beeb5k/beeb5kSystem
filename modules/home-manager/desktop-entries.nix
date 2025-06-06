18be1850d8ff3bbc74027888b73a27f058f3f1fd
beeb5k <beeb5k6@gmail.com>
2025-06-06 21:41:54 +0530

    update desktop entries to launch apps with wayland flags

--- modules/home-manager/desktop-entries.nix
+++ modules/home-manager/desktop-entries.nix
@@ -1,5 +1,7 @@
 { pkgs, ... }:
 {
+  /*
+
   xdg.dataFile."applications/foot-server.desktop".text = ''
     [Desktop Entry]
     Name=Foot Server
@@ -14,5 +16,41 @@
     Exec=${pkgs.foot}/bin/footclient
     Type=Application
     NoDisplay=true
+  '';
+
+  */
+
+  # vs code
+  xdg.dataFile."applications/code.desktop".text = ''
+    [Desktop Entry]
+    Actions=new-empty-window
+    Categories=Utility;TextEditor;Development;IDE
+    Comment=Code Editing. Redefined.
+    Exec=code --ozone-platform=wayland %F
+    GenericName=Text Editor
+    Icon=vscode
+    Keywords=vscode
+    Name=Visual Studio Code
+    StartupNotify=true
+    StartupWMClass=Code
+    Type=Application
+
+    [Desktop Action new-empty-window]
+    Exec=code --ozone-platform=wayland --new-window %F
+    Icon=vscode
+    Name=New Empty Window
+  '';
+
+  # vesktop
+  xdg.dataFile."applications/vesktop.desktop".text = ''
+    [Desktop Entry]
+    Categories=Network;InstantMessaging;Chat
+    Exec=vesktop --ozone-platform=wayland --enable-features=UseOzonePlatform %U
+    GenericName=Internet Messenger
+    Icon=vesktop
+    Keywords=discord;vencord;electron;chat
+    Name=Vesktop
+    StartupWMClass=Vesktop
+    Type=Application
   '';
 }
