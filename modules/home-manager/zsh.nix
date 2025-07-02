cb5c2ea522b6340a042e864c6dcecfcf5136d30c
beeb5k <beeb5k6@gmail.com>
2025-07-02 20:31:03 +0530

    Performance improvements

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -16,65 +16,59 @@
     };
 
     # Initialize Zinit and add it to ZSH
-    initContent = ''
-      pokeget random --hide-name
-      # Install Zinit if not already installed
-      if [[ ! -d $HOME/.zinit/bin ]]; then
-        mkdir -p $HOME/.zinit
-        git clone https://github.com/zdharma-continuum/zinit.git $HOME/.zinit/bin
-      fi
-
-      # ---- Source Zinit ----
-      source "$HOME/.zinit/bin/zinit.zsh"
-
-      # ---- Load plugins ----
-      zinit light zsh-users/zsh-autosuggestions
-
-      zinit ice wait"1" lucid
-      zinit light zsh-users/zsh-syntax-highlighting
-
-      zinit ice depth=1
-      zinit ice wait"1" lucid
-      zinit light jeffreytse/zsh-vi-mode
-
-      zinit ice wait"1" lucid
-      zinit light Aloxaf/fzf-tab
-
-      zinit ice wait"1" lucid
-      zinit light joshskidmore/zsh-fzf-history-search
-
-      zinit ice wait"1" lucid
-      zinit light mrjohannchang/zsh-interactive-cd
-
-      # ---- Keybindings ----
-      bindkey '^[[1;5D' backward-word      # Ctrl + Left
-      bindkey '^[[1;5C' forward-word       # Ctrl + Right
-      bindkey '^H' backward-kill-word      # Ctrl + Backspace
-      bindkey '^[[3;5~' kill-word          # Ctrl + Delete
-      bindkey '^[[C' forward-char          # Right Arrow
-      bindkey '^E' end-of-line             # Ctrl + E
-
-      # ---- Completion settings ----
-      zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
-      zstyle ':completion:*' rehash true
-      zstyle ':completion:*' menu select
-
-      # ---- Set options ----
-      setopt nocaseglob       # Case insensitive globbing
-      setopt correct          # Auto correct mistakes
-
-      clear_and_pokeget ()
-      {
-        print -Pn "\e[H\e[2J"
+    initContent = # bash
+      ''
         pokeget random --hide-name
-        zle reset-prompt
-      }
 
-      zle -N clear_and_pokeget
-      bindkey "^L" clear_and_pokeget
-    '';
+        if [[ ! -d "$HOME/.zinit/bin" ]]; then
+          mkdir -p "$HOME/.zinit"
+          git clone https://github.com/zdharma-continuum/zinit.git "$HOME/.zinit/bin"
+        fi
+
+        source "$HOME/.zinit/bin/zinit.zsh"
+
+        autoload -Uz compinit
+        compinit -C    # -C = use cache
+
+        eval "$(starship init zsh)"
+
+        zinit wait lucid light-mode depth=1 for \
+        zsh-users/zsh-autosuggestions \
+        Aloxaf/fzf-tab \
+        joshskidmore/zsh-fzf-history-search \
+        mrjohannchang/zsh-interactive-cd \
+        zsh-users/zsh-syntax-highlighting \
+        jeffreytse/zsh-vi-mode
+
+        zinit ice wait lucid light-mode depth=1 atload"_zsh_autosuggest_start"
+        zinit light zsh-users/zsh-autosuggestions
+
+        bindkey '^[[1;5D' backward-word      # Ctrl + Left
+        bindkey '^[[1;5C' forward-word       # Ctrl + Right
+        bindkey '^H' backward-kill-word      # Ctrl + Backspace
+        bindkey '^[[3;5~' kill-word          # Ctrl + Delete
+        bindkey '^[[C' forward-char          # Right Arrow
+        bindkey '^E' end-of-line             # Ctrl + E
+
+        zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+        zstyle ':completion:*' rehash true
+        zstyle ':completion:*' menu select
+
+        setopt nocaseglob     # Case-insensitive globbing
+        setopt correct        # Auto-correct small mistakes
+
+        clear_and_pokeget () {
+          print -Pn "\e[H\e[2J"
+            pokeget random --hide-name
+            zle reset-prompt
+        }
+
+        zle -N clear_and_pokeget
+          bindkey "^L" clear_and_pokeget
+      '';
 
     shellAliases = {
+      clear = "clear && pokeget random --hide-name";
       ls = "eza --icons --group-directories-first -1";
       ll = "eza --icons -lh --group-directories-first -1 --no-user --long";
       la = "eza --icons -lah --group-directories-first -1";
