a60d3311874c548d8ff060464fad0850f9ba7b2b
beeb5k <beeb5k6@gmail.com>
2025-08-22 17:44:36 +0530

    add nix-community cachix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,15 @@
 {
   description = "Flake for nix config.";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     beeb5kvim.url = "github:beeb5k/beeb5kvim";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -1,6 +1,13 @@
 { pkgs, ... }:
 {
   nix.settings = {
+    trusted-users = [ "@wheel" ];
+    extra-trusted-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
     experimental-features = [
       "nix-command"
       "flakes"
