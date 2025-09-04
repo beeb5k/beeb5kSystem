832192175dd8388131039ddb9cc9caeacb3fd1b1
beeb5k <beeb5k6@gmail.com>
2025-09-04 22:41:55 +0530

    update tmux config

--- modules/home/programs/tmux.nix
+++ modules/home/programs/tmux.nix
@@ -3,11 +3,16 @@
     enable = true;
     extraConfig = ''
       set -g status-position top
+      set -g status-justify absolute-centre
+      set -g status-right ""
+      set -g status-left "#S"
+      set -g base-index 1
+      set -g renumber-windows on
+      set -g mode-key vi
       set -sg escape-time 0
       set-option -g status-style bg=default
-      unbind C-b
-      set -g prefix C-a
-      bind C-a send-prefix
+      set-option -g window-status-current-style "fg=blue bold"
+      set -g prefix C-SPACE
     '';
   };
 }
