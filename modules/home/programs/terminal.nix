42906c7db936e2d054fe3aa24572eaf3cf060414
beeb5k <beeb5k6@gmail.com>
2025-10-14 23:48:28 +0530

    alacritty as default terminal

--- modules/home/programs/terminal.nix
+++ modules/home/programs/terminal.nix
@@ -1,13 +1,14 @@
 {
   programs.foot = {
-    enable = true;
+    enable = false;
     settings = {
       main = {
         pad = "10x10";
         dpi-aware = "no";
         bold-text-in-bright = "no";
         # font = "Maple Mono NF:size=13";
-        font = "Hack Nerd Font:size=13";
+        # font = "Hack Nerd Font:size=13";
+        font = "Iosevka Term Nerd Font:size=12";
       };
 
       # cursor = {
@@ -15,6 +16,58 @@
       #   blink = "no";
       #   beam-thickness = 1;
       # };
+    };
+  };
+
+  programs.ghostty = {
+    enable = false;
+    settings = {
+      font-family = "Iosevka Term Nerd Font";
+      font-feature = [
+        "calt"
+        "liga"
+        "dlig"
+      ];
+      font-size = 12;
+      window-padding-x = 10;
+      window-padding-y = 10;
+      bold-is-bright = false;
+      gtk-titlebar = false;
+      gtk-single-instance = false;
+      gtk-tabs-location = "bottom";
+      gtk-wide-tabs = false;
+      resize-overlay = "never";
+      copy-on-select = false;
+      confirm-close-surface = false;
+      mouse-hide-while-typing = true;
+    };
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window = {
+        padding = {
+          y = 10;
+          x = 10;
+        };
+        dimensions = {
+          lines = 75;
+          columns = 100;
+        };
+      };
+
+      # cursor = {
+      #   style = {
+      #     shape = "Beam";
+      #   };
+      #   thickness = 0.0;
+      # };
+
+      font = {
+        normal.family = "Iosevka Term Nerd Font";
+        size = 12.5;
+      };
     };
   };
 }
