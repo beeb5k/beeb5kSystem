39b524c213aad9e571b4ef05749f7c23717a975e
beeb5k <beeb5k6@gmail.com>
2025-10-23 20:36:51 +0530

    Neopywal matugen template

--- modules/home/programs/matugen/default.nix
+++ modules/home/programs/matugen/default.nix
@@ -70,6 +70,10 @@
           [templates.btop]
           input_path = '~/beeb5kSystem/modules/home/programs/matugen/templates/matugen-btop.theme'
           output_path = '~/.config/btop/themes/matugen.theme'
+
+          [templates.btop]
+          input_path = '~/beeb5kSystem/modules/home/programs/matugen/templates/neopywal.vim'
+          output_path = '~/.cache/wal/colors-wal.vim'
         '';
     };
   };
