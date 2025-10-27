3a2186edbd75e18ab442e0a9db432cac9977f714
beeb5k <beeb5k6@gmail.com>
2025-10-27 16:28:03 +0530

    fix hyprlock color variable names

--- modules/home/programs/hyprland/hyprlock.nix
+++ modules/home/programs/hyprland/hyprlock.nix
@@ -7,7 +7,7 @@
       background = {
         monitor = "";
         # path = "$image";
-        color = "$background";
+        color = "$_background";
         # blur_passes = 2;
         # contrast = 1;
         # brightness = 0.5;
@@ -33,13 +33,13 @@
         dots_size = 0.2; # Scale of input-field height, 0.2 - 0.8
         dots_spacing = 0.35; # Scale of dots' absolute size, 0.0 - 1.0
         dots_center = true;
-        outer_color = "$outline";
-        inner_color = "$surface_container";
-        font_color = "$primary";
+        outer_color = "$_outline";
+        inner_color = "$_surface_container";
+        font_color = "$_primary";
         fade_on_empty = false;
         rounding = -1;
-        check_color = "$on_secondary_container";
-        placeholder_text = "<i><span foreground='##cdd6f4'>Enter Password...</span></i>";
+        check_color = "$_on_secondary_container";
+        # placeholder_text = "<i><span foreground='##cdd6f4'>Enter Password...</span></i>";
         hide_input = false;
         position = "0, -200";
         halign = "center";
@@ -50,7 +50,7 @@
         {
           monitor = "";
           text = "cmd[update:1000] echo \"$(date +\"%A, %B %d\")\"";
-          color = "$on_background";
+          color = "$_on_background";
           font_size = 22;
           font_family = "JetBrains Mono";
           position = "0, 300";
@@ -61,7 +61,7 @@
         {
           monitor = "";
           text = "cmd[update:1000] echo \"$(date +\"%-I:%M\")\"";
-          color = "$on_background";
+          color = "$_on_background";
           font_size = 95;
           font_family = "JetBrains Mono Extrabold";
           position = "0, 200";
